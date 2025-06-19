c739e5ccbaf6f8efa99a1e8df7a4e2e24e6819c6
FevarVates <arsen.fox.00@mail.ru>
2025-06-20 02:34:45 +0400

    Add files via upload

---
+++ TouristAgentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TouristAgent.Models;
+
+namespace TouristAgent.DateBase;
+
+public class DbConnectionContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Master> Masters { get; set; }
+    public DbSet<RecordOfService> RecordOfServicess { get; set; }
+    public DbSet<Service> Servicess { get; set; }
+    
+    public DbSet<Back> Backs { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite("Data Source=TouristAgent.db");
+    }
+}
