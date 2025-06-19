77d85a6066fcca1421704feb5b224531f85a3b05
FevarVates <arsen.fox.00@mail.ru>
2025-06-20 02:37:29 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using TouristAgent.Components;
+using TouristAgent.DateBase;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<DbConnectionContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DbConnectionContext>();
+    context.Database.Migrate();
+}
+
+app.Run();
